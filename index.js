16efdbc4c33f2328b29a1b8d0e25bd3b9a6a1d46
elenems <mickhaelyarish@gmail.com>
2020-03-08 17:04:34 +0200

    Open connection on the server

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const socket = require('socket.io');
 // App setup
 const app = express();
 const server = app.listen(3000, () => {
     console.log('Listening 3000');
 });
+
+app.use(express.static('public'));
+
+// Socket setup
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('made socket connection');
+})
