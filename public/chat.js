697646ba078e103de73a72a809d37dcfcad64090
elenems <mickhaelyarish@gmail.com>
2020-03-08 18:25:04 +0200

    Add typing status

--- public/chat.js
+++ public/chat.js
@@ -3,12 +3,22 @@ const socket = io.connect('http://localhost:3000');
 const message = document.getElementById('message'),
   handle = document.getElementById('handle'),
   btn = document.getElementById('send'),
-  output = document.getElementById('output');
+  output = document.getElementById('output'),
+  typing = document.getElementById('typing');
 
 btn.addEventListener('click', () => {
   socket.emit('chat', { message: message.value, handle: handle.value });
 });
 
+message.addEventListener('keypress', () => {
+  socket.emit('typing', handle.value);
+});
+
 socket.on('chat', (data) => {
-    output.innerHTML += `<p><strong>${data.handle} : </strong> ${data.message} </p>`
+  typing.innerHTML = '';
+  output.innerHTML += `<p><strong>${data.handle} : </strong> ${data.message} </p>`;
+});
+
+socket.on('typing', (data) => {
+  typing.innerHTML = `<p>${data} is typing...</p>`;
 });
